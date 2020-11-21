582b5af0563910c1e70360118a7526afb6a45e94
cmcode <code@cmcode.dev>
2020-11-21 01:06:16 +0000

    feat: v0.0.3 bulk rename/duplicate mode, cleanup

---
+++ web-ext.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -e
+
+web-ext build -s "./src" --overwrite-dest
+
+echo -n "Enter your JWT issuer key from addons.mozilla.org: "
+read API_KEY
+
+echo -n "Enter your JWT secret from addons.mozilla.org: "
+read -s API_SECRET
+echo
+
+echo -n "Enter the extension ID: "
+read ADDON_ID
+echo
+
+web-ext sign -s "./src" --api-key "${API_KEY}" --api-secret "${API_SECRET}" --id "${ADDON_ID}"
