6dd9d62c908d09abcba0ff6e1a453536e68b740f
cmcode <code@cmcode.dev>
2021-03-20 17:53:37 -0700

    fix: remove id from web-ext.sh since we have it in manifest.json now

--- web-ext.sh
+++ web-ext.sh
@@ -9,8 +9,8 @@ echo -n "Enter your JWT secret from addons.mozilla.org: "
 read -s API_SECRET
 echo
 
-echo -n "Enter the extension ID: "
-read ADDON_ID
-echo
+# echo -n "Enter the extension ID: "
+# read ADDON_ID
+# echo
 
-web-ext sign -s "./src" --api-key "${API_KEY}" --api-secret "${API_SECRET}" --id "${ADDON_ID}"
+web-ext sign -s "./src" --api-key "${API_KEY}" --api-secret "${API_SECRET}" # --id "${ADDON_ID}"
